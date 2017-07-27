f3cee40423561410d1eb2e3e30ba2d575bae2bb4
Neil Opena <neil1114o@gmail.com>
2017-07-27 15:33:38 -0400

    resumeBuilder updated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Neil");
+var formattedName = HTMLheaderName.replace("%data%","Neil Opena");
+var formattedRole = HTMLheaderRole.replace("%data%","Software Engineer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
