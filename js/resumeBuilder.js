4e7c636a434830fdbc2ff874fc3311efad95bb8c
Neil Opena <neil1114o@gmail.com>
2017-07-27 21:23:00 -0400

    added objects for resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,54 @@
-var formattedName = HTMLheaderName.replace("%data%","Neil Opena");
-var formattedRole = HTMLheaderRole.replace("%data%","Software Engineer");
+// var formattedName = HTMLheaderName.replace("%data%","Neil Opena");
+// var formattedRole = HTMLheaderRole.replace("%data%","Software Engineer");
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var bio = {
+    "name":"Neil Opena",
+    "role":"Software Engineer",
+    "contacts":{
+        "mobile":"516-304-4803",
+        "email":"neil1114o@gmail.com",
+        "github":"Neil1114",
+        "location":"New York"
+    },
+    "welcomeMessage": "Random message here",
+    "skills":["awesomeness","delivering things","cryogenic sleep","saving the universe"
+    ],
+    "bioPic":"images/fry.jpg"
+}
+
+// var work = {};
+// work.position = "Course Developer";
+// work.employer = "Udacity";
+// work.years = 0.3;
+
+var education = [
+    {
+        "name":"Stony Brook University",
+        "location":"Stony Brook, New York",
+        "degree":"B.S.",
+        "majors":["CS","AMS"],
+        "dates":2016
+    }
+
+];
+
+var work = [
+    {
+        "employer":"Ocean Beach Park",
+        "title":"Cashier",
+        "location":"Long Beach, NY",
+        "dates":"Jun 2016 - present",
+        "description":"Handled money"
+    }
+];
+
+var projects = [
+    {
+        "title":"Personal Website",
+        "dates":"Summer '17",
+        "description":"Made a personal website"
+    }
+]
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
